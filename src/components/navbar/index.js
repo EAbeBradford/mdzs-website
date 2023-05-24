e76c9e5020d8f2d01141d56ab20841c201e0f4f3
Mx. Bradford <emma_bradford@milton.com>
2023-05-24 14:23:12 -0400

    made small changes

---
+++ src/components/navbar/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Nav, NavLink, NavMenu }
+    from "./navBarElements";
+
+const Navbar = () => {
+    return (
+ 
+        
+
+        <div className="NavBar">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="/">MDZS API</a>
+                    </div>
+                    <ul class="nav navbar-nav">
+
+                    <NavLink to="/" activeStyle>
+                        Random Char
+                    </NavLink>
+                    {/* <NavLink to="/waterTribe" activeStyle>
+                        Water Tribe Charater (Random)
+                    </NavLink>
+                    <NavLink to="/fireNation" activeStyle>
+                        Fire Nation Charater (Random)
+                    </NavLink>
+                    <NavLink to="/earthKingdom" activeStyle>
+                        Earth Kingdom Charater (Random)
+                    </NavLink>
+                    <NavLink to="/airNomads" activeStyle>
+                        Air Nomads Charater (Random)
+                    </NavLink> */}
+                    </ul>
+                </div>
+            </nav >
+        </div >
+
+    );
+};
+
+export default Navbar;
---
+++ src/pages/WeiYing.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+const WeiYing = () => {
+  const [Data, setData] = useState({
+    birthName: '',
+    courtesyName: '',
+    title: '',
+    sect: '',
+    weapon: [],
+    picture: '',
+
+
+  })
+  useEffect(() => {
+    axios.get('https://mdzs-api.fly.dev/characters/644fffc6ad59705a8ee36956')
+      .then(res => {
+        console.log('Response from main API: ', res)
+        console.log('Wei Ying Data: ', res.data)
+        // let index = Math.floor(Math.random()* res.data.length)
+        let WeiYingData = res.data;
+        
+        
+        setData({ birthName: WeiYingData.birthName, 
+            courtesyName: WeiYingData.courtesyName, 
+            title: WeiYingData.title, 
+          allies: WeiYingData.allies, 
+          sect: WeiYingData.sect, 
+          weapon: WeiYingData.weapon,
+          picture: WeiYingData.picture
+         })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+ 
+  return (
+
+    <div>
+      <h1> Birth Name: {Data.birthName}</h1>
+      <h1> Courtesy Name: {Data.courtesyName}</h1>
+      <h1> Title: {Data.title}</h1>
+      <h1> Sect: {Data.sect}</h1>
+      <h1> Weapon: {Data.weapon}</h1>
+
+      <img src={Data.picture} alt={Data.birthName} class='scale-down'/>
+
+      
+
+    </div>
+  );
+};
+
+export default WeiYing;
